441c1f5727396765e9b1d392cc6f66137e90a02e
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2020-07-14 18:24:10 -0300

    Exercises 4.2

---
+++ 4_2/exercise06.js
@@ -0,0 +1,14 @@
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let oddCounter = 0;
+
+for (index in numbers) {
+
+  if (numbers[index] % 2 != 0) {
+    oddCounter += 1;
+  }
+
+}
+
+console.log(oddCounter)
